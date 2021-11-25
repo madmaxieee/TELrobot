5d954e8a7a00ebf83d62c57645c2dbc01894a3f1
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2021-11-26 04:09:32 +0800

    trivial

--- test/camera.py
+++ test/camera.py
@@ -1,15 +1,25 @@
-import numpy as np
-import cv2 as cv
-cap = cv.VideoCapture('vtest.avi')
-while cap.isOpened():
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv.destroyAllWindows()
+# import numpy as np
+# import cv2 as cv
+# cap = cv.VideoCapture('vtest.avi')
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     # if frame is read correctly ret is True
+#     if not ret:
+#         print("Can't receive frame (stream end?). Exiting ...")
+#         break
+#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+#     cv.imshow('frame', gray)
+#     if cv.waitKey(1) == ord('q'):
+#         break
+# cap.release()
+# cv.destroyAllWindows()
+import cv2
+
+videoCaptureObject = cv2.VideoCapture(0)
+result = True
+while(result):
+    ret, frame = videoCaptureObject.read()
+    cv2.imwrite("NewPicture.jpg", frame)
+    result = False
+videoCaptureObject.release()
+cv2.destroyAllWindows()
