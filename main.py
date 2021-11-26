9555f4172ed332904ce6a5a74c72a247112b9a45
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2021-11-27 06:25:46 +0800

    -

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from controllers.DashBoard import DashBoard
 from auto import Auto
+from subsystems import base
 
 def main():
     if len(sys.argv) == 1:
@@ -10,7 +11,9 @@ def main():
         if sys.argv[1] == 'auto':
             a = Auto()
             a.start()
-
+        elif sys.argv[1] == 'clean':
+            base.clearDriveSpeeds()
+            exit()
 
 if __name__ == '__main__':
     main()
