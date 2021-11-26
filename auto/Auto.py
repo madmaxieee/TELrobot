a372ef5284fdb4479f93d493959e5024b3a578e9
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2021-11-27 06:13:52 +0800

    -

--- auto/Auto.py
+++ auto/Auto.py
@@ -3,14 +3,20 @@ import numpy as np
 from subsystems import base
 from tel_image import findDoorCenter, vision
 from config import params
+from time import sleep
 
 center_x = params["cam_size"][0] / 2
+
 
 class Auto:
     def __init__(self) -> None:
         self.running = False
         self.thread = Thread(target=self.loop)
         # self.stages = [self.stage1, self.stage2, self.stage3]
+        self.door_ind = 0
+        self.center_list = []
+        self.avg_center = -1
+        self.onTrack = False
 
     def start(self):
         self.running = True
@@ -25,14 +31,29 @@ class Auto:
             base.clearDriveSpeeds()
 
     def stage1(self):
-        doors = ['red', 'green', 'blue']
-        door_center = findDoorCenter(vision.readCam(), doors[0])
-        print(door_center)
-        if door_center[0] == -1:
+        doors = ('red', 'green', 'blue')
+        door_center = findDoorCenter(vision.readCam(), doors[self.door_ind])
+        if len(self.center_list) == 0:
+            for _ in range(10):
+                while True:
+                    door_center = findDoorCenter(
+                        vision.readCam(), doors[self.door_ind])
+                    if door_center[0] != -1:
+                        self.center_list.append(door_center[0])
+                        break
+            self.avg_center = sum(self.center_list) / len(self.center_list)
+        elif len(self.center_list) == 10 and not self.onTrack:
+            self.center_list = []
+            if self.avg_center < 300:
+                base.cartDrive((0, 1), spin=0.3)
+            elif self.avg_center > 340:
+                base.cartDrive((0, -1), spin=-0.3)
+            else:
+                base.cartDrive((1, 0))
+            sleep(1)
+            self.onTrack = True
+        elif self.onTrack:
             base.cartDrive((1, 0))
-        elif door_center[0] < 300:
-            base.cartDrive((0, 1), spin=0.3)
-        elif door_center[0] > 340:
-            base.cartDrive((0, -1), spin=-0.3)
-        else:
-            base.cartDrive((1, 0))
+            sleep(5)
+            self.door_ind += 1
+            self.onTrack = False
