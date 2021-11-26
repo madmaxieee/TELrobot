8460e1d59b07f8b5d10889a5e6e4c0bba2223add
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2021-11-27 04:29:45 +0800

    -

--- auto/Auto.py
+++ auto/Auto.py
@@ -1,10 +1,12 @@
 from threading import Thread
 import numpy as np
 from subsystems import base
-from tel_image import findDoorCenter
+from tel_image import findDoorCenter, vision
 from config import params
 
 center_x, _ = params["cam_size"] / 2
+
+
 class Auto:
     def __init__(self) -> None:
         self.running = False
@@ -20,8 +22,15 @@ class Auto:
 
     def loop(self):
         while self.running:
-            pass
+            self.stage1()
+            base.clearDriveSpeeds()
 
     def stage1(self):
-        
-        pass
+        doors = ['red', 'green', 'blue']
+        door_center = findDoorCenter(vision.readCam(), doors[0])
+        if door_center[0] < 300:
+            base.cartDrive((0, 1), spin=1)
+        elif door_center[0] > 340:
+            base.cartDrive((0, -1), spin=-1)
+        else:
+            base.cartDrive((1, 0))
