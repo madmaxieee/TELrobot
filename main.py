6c6c45cab05c804306efb7434bf90117762e6ab1
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2021-11-27 05:05:27 +0800

    -

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     if len(sys.argv) == 1:
         dash = DashBoard()
         dash.start()
-    elif len(sys.argv > 1):
+    elif len(sys.argv) > 1:
         if sys.argv[1] == 'auto':
             print('auto')
 
